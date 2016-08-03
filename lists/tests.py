548ad17df77254b8f6218df5c2ca7f8f01e273e1
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-03 22:56:18 +0100

    new URLs for unique lists and for adding to existing lists. FT Passes

--- lists/tests.py
+++ lists/tests.py
@@ -36,24 +36,66 @@ class NewListTest(TestCase):
             '/lists/new',
             data={'item_text': 'A new list item'}
         )
-        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
+        new_list = List.objects.first()
+        self.assertRedirects(response, '/lists/{}/'.format(new_list.id))
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data={'item_text': 'New Item for Existing List'}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'New Item for Existing List')
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/{}/add_item'.format(correct_list.id),
+            data={'item_text': 'New Item for Existing List'}
+        )
+
+        self.assertRedirects(response, '/lists/{}/'.format(correct_list.id))
 
 
 class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+        list_ = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(list_.id))
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_displays_all_list_items(self):
-        list_ = List.objects.create()
-        Item.objects.create(text='First and foremost', list=list_)
-        Item.objects.create(text='Second but important', list=list_)
+    def test_passes_correct_list_to_template(self):
+        List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/{}/'.format(correct_list.id))
+        self.assertEqual(response.context['list'], correct_list)
 
-        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+    def test_displays_only_items_for_that_list(self):
+        first_list = List.objects.create()
+        Item.objects.create(text='First and foremost', list=first_list)
+        Item.objects.create(text='Second but important', list=first_list)
+        second_list = List.objects.create()
+        Item.objects.create(text='Foo', list=second_list)
+        Item.objects.create(text='Bar', list=second_list)
+
+        response = self.client.get('/lists/{}/'.format(first_list.id))
 
         self.assertContains(response, 'First and foremost')
         self.assertContains(response, 'Second but important')
+        self.assertNotContains(response, 'Foo')
+        self.assertNotContains(response, 'Bar')
 
 
 class ListAndItemModelTest(TestCase):
