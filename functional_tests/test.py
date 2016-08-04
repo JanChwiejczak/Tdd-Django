9395a2df77a31f37569b8d19ef02ce012d786956
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-04 22:47:17 +0100

    Add Bootstrap to prettify site and imporve layout

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,9 +1,9 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -107,6 +107,4 @@ class NewVisitorTest(LiveServerTestCase):
             512,
             delta=10
         )
-
-
 
