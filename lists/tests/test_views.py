12bf90c0543bd9e818748f78a3ec9fb0c22df2dd
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-12 14:47:39 +0100

    Use new form in home page, simplify tests. NB breaks FTs

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -6,19 +6,18 @@ import pytest
 
 from lists.models import Item, List
 from lists.views import home_page
+from lists.forms import ItemForm
 
 
 class TestHomePage:
 
-    def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        assert found.func == home_page
+    def test_home_page_renders_home_template(self, client):
+        response = client.get('/')
+        assert 'home.html' == response.templates[0].name
 
-    def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        expected_html = render_to_string('home.html', request=request)
-        assert response.content.decode() == expected_html
+    def test_home_page_uses_item_form(self, client):
+        response = client.get('/')
+        assert isinstance(response.context['form'], ItemForm)
 
 
 @pytest.mark.django_db
