77a9892104a7ebfe098632cbf32c1c28b81ed5c0
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-12 11:33:55 +0100

    Port and refactor model tests to pytest

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -1,47 +1,43 @@
 from django.core.exceptions import ValidationError
-from django.test import TestCase
+import pytest
+
 from lists.models import Item, List
 
 
-class ListAndItemModelTest(TestCase):
+@pytest.mark.django_db
+class TestListAndItemModel:
 
     def test_saving_and_retrieving_items(self):
-        list_ = List()
-        list_.save()
-
-        first_item = Item()
-        first_item.text = 'The first (ever) list item'
-        first_item.list = list_
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = 'Item the second'
-        second_item.list = list_
-        second_item.save()
+        list_ = List.objects.create()
+        first_item = Item.objects.create(
+            text='The first (ever) list item', list=list_
+        )
+        second_item = Item.objects.create(
+            text='Item the second', list=list_
+        )
 
         saved_list = List.objects.first()
-        self.assertEqual(saved_list, list_)
+        assert saved_list == list_
 
         saved_items = Item.objects.all()
-        self.assertEqual(saved_items.count(), 2)
+        assert saved_items.count() == 2
 
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, first_item.text)
-        self.assertEqual(first_saved_item.list, list_)
-        self.assertEqual(second_saved_item.text, second_item.text)
-        self.assertEqual(second_saved_item.list, list_)
+        assert first_saved_item.text == first_item.text
+        assert first_saved_item.list == list_
+        assert second_saved_item.text == second_item.text
+        assert second_saved_item.list == list_
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
         item = Item(text='', list=list_)
-        with self.assertRaises(ValidationError):
+        with pytest.raises(ValidationError):
             item.save()
             item.full_clean()
 
     def test_get_absolute_url(self):
         list_ = List.objects.create()
-        self.assertEqual(list_.get_absolute_url(), '/lists/{}/'.format(list_.id))
-
-
+        expected_url = '/lists/{}/'.format(list_.id)
+        assert list_.get_absolute_url() == expected_url
 
