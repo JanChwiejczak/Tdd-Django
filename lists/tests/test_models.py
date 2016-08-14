9a160c289c8d82976ff8a7417b632222b578ed1a
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-14 22:56:02 +0100

    New FT for duplicate items, model tests refactor

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -5,29 +5,18 @@ from lists.models import Item, List
 
 
 @pytest.mark.django_db
-class TestListAndItemModel:
+class TestItemModel:
 
-    def test_saving_and_retrieving_items(self):
+    def test_default_text(self):
+        item = Item()
+        assert item.text == ''
+
+    def test_item_is_related_to_list(self):
         list_ = List.objects.create()
-        first_item = Item.objects.create(
-            text='The first (ever) list item', list=list_
-        )
-        second_item = Item.objects.create(
-            text='Item the second', list=list_
-        )
-
-        saved_list = List.objects.first()
-        assert saved_list == list_
-
-        saved_items = Item.objects.all()
-        assert saved_items.count() == 2
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        assert first_saved_item.text == first_item.text
-        assert first_saved_item.list == list_
-        assert second_saved_item.text == second_item.text
-        assert second_saved_item.list == list_
+        item = Item()
+        item.list = list_
+        item.save()
+        assert item in list_.item_set.all()
 
     def test_cannot_save_empty_list_items(self):
         list_ = List.objects.create()
@@ -61,8 +50,11 @@ class TestListAndItemModel:
         item = Item(text='some text')
         assert str(item) == 'some text'
 
+
+@pytest.mark.django_db
+class TestListModel:
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         expected_url = '/lists/{}/'.format(list_.id)
         assert list_.get_absolute_url() == expected_url
-
