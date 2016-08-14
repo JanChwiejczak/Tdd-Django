31e6c6908037b0b6a0bb4da2176fa46e0514d551
Jan Chwiejczak <jchwiejczak@gmail.com>
2016-08-14 22:36:02 +0100

    new validation for duplicate items at model level

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -36,6 +36,31 @@ class TestListAndItemModel:
             item.save()
             item.full_clean()
 
+    def test_duplicate_items_are_invalid(self):
+        list_ = List.objects.create()
+        Item.objects.create(text='First', list=list_)
+        with pytest.raises(ValidationError):
+            item = Item(text='First', list=list_)
+            item.full_clean()
+
+    def test_can_save_same_item_to_different_lists(self):
+        list1 = List.objects.create()
+        list2 = List.objects.create()
+        Item.objects.create(text='First', list=list1)
+        item = Item(text='First', list=list2)
+        item.full_clean()
+
+    def test_list_ordering(self):
+        list_ = List.objects.create()
+        item1 = Item.objects.create(text='item 1', list=list_)
+        item2 = Item.objects.create(text='item 2', list=list_)
+        item3 = Item.objects.create(text='item 3', list=list_)
+        assert list(Item.objects.all()) == [item1, item2, item3]
+
+    def test_string_representation(self):
+        item = Item(text='some text')
+        assert str(item) == 'some text'
+
     def test_get_absolute_url(self):
         list_ = List.objects.create()
         expected_url = '/lists/{}/'.format(list_.id)
